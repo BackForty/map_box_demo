a1408d50b224f36bba74caf19d99bd4256622e82
Flip Sasser <flip@x451.com>
2013-01-29 09:22:51 -0500

    Initial Commit

---
+++ MapBoxDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapBoxDemo
+//
+//  Created by Flip Sasser on 1/29/13.
+//  Copyright (c) 2013 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapBoxDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MapBoxDemo
+//
+//  Created by Flip Sasser on 1/29/13.
+//  Copyright (c) 2013 Back Forty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
